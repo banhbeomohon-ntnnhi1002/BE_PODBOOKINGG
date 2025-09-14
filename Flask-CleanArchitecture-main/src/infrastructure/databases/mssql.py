624f7fe33a170f1a8e1fc697cbee2bb6c8c14c38
legiahuy180406 <huylg5065@ut.edu.vn>
2025-09-14 22:56:04 +0700

    Thêm tính năng quản lý tài khoản và các API

--- Flask-CleanArchitecture-main/src/infrastructure/databases/mssql.py
+++ Flask-CleanArchitecture-main/src/infrastructure/databases/mssql.py
@@ -4,19 +4,9 @@ from config import Config
 from infrastructure.databases.base import Base
 
 # Database configuration
-DATABASE_URL = Config.DATABASE_URI
-print(DATABASE_URL)
-# Create a new SQLAlchemy engine instance
-engine = create_engine(DATABASE_URL)
-
-# Create a configured "Session" class
+DATABASE_URI = Config.DATABASE_URI
+engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+session = SessionLocal()
 def init_mssql(app):
-    # Import all modules here that might define models so that
-    # they will be registered properly on the metadata.
-    # This is necessary to avoid circular imports.
-    
-
-    # Create the database tables
     Base.metadata.create_all(bind=engine)
